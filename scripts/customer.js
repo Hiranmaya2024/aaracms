9248ff97f74620259af145011c9a0ec283dba0d0
Hiranmaya2024 <mohapatrahiranmaya@gmail.com>
2024-12-24 20:19:44 +0530

    Update customer.js

--- scripts/customer.js
+++ scripts/customer.js
@@ -1,13 +1,14 @@
+document.addEventListener('DOMContentLoaded', async () => 
+    {
+    const ledgerTable = document.getElementById('ledgerTable');
+    const username = sessionStorage.getItem('username');
 // Check authentication
 if (!sessionStorage.getItem('isAuthenticated') || sessionStorage.getItem('userType') !== 'customer') 
 {
     window.location.href = '../index.html';
 }
 
-document.addEventListener('DOMContentLoaded', async () => 
-    {
-    const ledgerTable = document.getElementById('ledgerTable');
-    const username = sessionStorage.getItem('username');
+
      // Load customer ledger
         const ledger = await window.getLedger();
         console.log(ledger);
