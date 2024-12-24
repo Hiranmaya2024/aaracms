8ed3fd747cf223c97bb264e5f05658f3d4b42a00
Hiranmaya2024 <mohapatrahiranmaya@gmail.com>
2024-12-24 19:44:34 +0530

    Update customer.js

--- scripts/customer.js
+++ scripts/customer.js
@@ -6,26 +6,13 @@ if (!sessionStorage.getItem('isAuthenticated') || sessionStorage.getItem('userTy
 
 document.addEventListener('DOMContentLoaded', async () => 
     {
-   // const offersList = document.getElementById('offersList');
     const ledgerTable = document.getElementById('ledgerTable');
     const username = sessionStorage.getItem('username');
-
-  // try {
-        // Load offers
-  //      const offers = await window.getOffers();
- //       offers.forEach(offer => {
-   //         const li = document.createElement('li');
- //           li.textContent = offer[0];
-       //    offersList.appendChild(li);
- //      });
-
-        // Load customer ledger
-       // const ledger = await window.getCustomerLedger;
+     // Load customer ledger
         const ledger = await window.getLedger();
         console.log(ledger);
         const userLedger = ledger.filter(row => row[0] === username);
-        
-        userLedger.forEach(row => 
+         userLedger.forEach(row => 
             {
             const tr = document.createElement('tr');
             row.forEach(cell => 
@@ -37,9 +24,4 @@ document.addEventListener('DOMContentLoaded', async () =>
             ledgerTable.appendChild(tr);
            });
     paginateTable('ledgerTable', 10); // Apply pagination
-        /*catch (error) 
-        {
-        console.error('Error loading customer data:', error);
-        alert('Error loading data. Please try again later.');
-        }*/
-});
+       });
