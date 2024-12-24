3453f698177fc24ca5a592320d977cfc49e6425c
Hiranmaya2024 <mohapatrahiranmaya@gmail.com>
2024-12-24 19:25:17 +0530

    Update staff.js

--- scripts/staff.js
+++ scripts/staff.js
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', async () => {
                 tr.appendChild(td);
             });
             stockTable.appendChild(tr);
-        });
+   //     });
 
         // Load all customer ledgers
-        const customerLedger = await window.getCustomerLedger();
-        customerLedger.forEach(row => {
-            const tr = document.createElement('tr');
-            row.forEach(cell => {
-                const td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-            });
+     //   const customerLedger = await window.getCustomerLedger();
+     //   customerLedger.forEach(row => {
+       //     const tr = document.createElement('tr');
+        //    row.forEach(cell => {
+          //      const td = document.createElement('td');
+        //        td.textContent = cell;
+        //        tr.appendChild(td);
+        //    });
             customerLedgerTable.appendChild(tr);
         });
   paginateTable('pendingPaymentsTable', 10); // Apply pagination
