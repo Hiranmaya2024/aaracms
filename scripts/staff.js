485b1e1583c881329aa92824094a88cb31aaaff8
Hiranmaya2024 <mohapatrahiranmaya@gmail.com>
2024-12-25 15:29:54 +0530

    Update staff.js

--- scripts/staff.js
+++ scripts/staff.js
@@ -1,40 +1,38 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const stockTable = document.getElementById('stockTable');
     const customerLedgerTable = document.getElementById('customerLedgerTable');
+    const paginationContainerStock = document.getElementById('paginationContainerStock');
+    const paginationContainerCustomer = document.getElementById('paginationContainerCustomer');
 
     // Check authentication
-    if (!sessionStorage.getItem('isAuthenticated') || sessionStorage.getItem('userType') !== 'staff') 
-    {
+    if (!sessionStorage.getItem('isAuthenticated') || sessionStorage.getItem('userType') !== 'staff') {
         window.location.href = '../index.html';
-        return; // Important: Exit the function early after redirection
+        return;
     }
 
-    
-        // Load stock data
-        const stockData = await window.getStockData();
-        stockData.forEach(row => 
-            {
-            const tr = document.createElement('tr');
-            row.forEach(cell => {
-                const td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-            });
-            stockTable.appendChild(tr);
-            });
-      paginateTable('stockTable', 10); // Apply pagination
+    // Load stock data
+    const stockData = await window.getStockData();
+    stockData.forEach(row => {
+        const tr = document.createElement('tr');
+        row.forEach(cell => {
+            const td = document.createElement('td');
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+        stockTable.querySelector('tbody').appendChild(tr);
+    });
+    paginateTable(stockTable, paginationContainerStock, 10); // Apply pagination
 
-   // Load all customer ledgers
-        const customerLedger = await window.getCustomerLedger();
-        customerLedger.forEach(row => {
-            const tr = document.createElement('tr');
-            row.forEach(cell => {
-                const td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-            });
-            customerLedgerTable.appendChild(tr);
-            });
-     paginateTable('customerpendingTable', 10); // Apply pagination
-
- });
+    // Load customer ledger data
+    const customerLedger = await window.getCustomerLedger();
+    customerLedger.forEach(row => {
+        const tr = document.createElement('tr');
+        row.forEach(cell => {
+            const td = document.createElement('td');
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+        customerLedgerTable.querySelector('tbody').appendChild(tr);
+    });
+    paginateTable(customerLedgerTable, paginationContainerCustomer, 10); // Apply pagination
+});
